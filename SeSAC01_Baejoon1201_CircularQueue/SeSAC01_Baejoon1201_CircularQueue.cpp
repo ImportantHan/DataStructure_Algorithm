69f16905338e8fec24eaf24b2ba464cc46f295dc
한정무 <99253941+ImportantHan@users.noreply.github.com>
2023-10-14 20:25:52 +0900

    Baejoon1201_CircularQueue 성공

--- SeSAC01_Baejoon1201_CircularQueue/SeSAC01_Baejoon1201_CircularQueue.cpp
+++ SeSAC01_Baejoon1201_CircularQueue/SeSAC01_Baejoon1201_CircularQueue.cpp
@@ -1,20 +1,184 @@
-// SeSAC01_Baejoon1201_CircularQueue.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+class Node {
+private:
+    int Data;
+    Node* NextNode;
+    Node* PrevNode;
+public:
+    Node(int NewData) {
+        Data = NewData;
+        NextNode = nullptr;
+        PrevNode = nullptr;
+    }
+    // Node(int NewData) : Data(NewData), NextNode(nullptr), PrevNode(nullptr) {}
+
+    ~Node() {
+
+    }
+
+    int GetData() {
+        return Data;
+    }
+
+    Node* GetNextNode() {
+        return NextNode;
+    }
+
+    Node* GetPrevNode() {
+        return PrevNode;
+    }
+
+    int SetData(int NewData) {
+        Data = NewData;
+    }
+
+    void SetNextNode(Node* NewNextNode) {
+        NextNode = NewNextNode;
+    }
+
+    void SetPrevNode(Node* NewPrevNode) {
+        PrevNode = NewPrevNode;
+    }
+};
+
+class CDLL {
+private:
+    Node* Head;
+    int Size;
+public:
+    CDLL() {
+        Head = nullptr;
+        Size = 0;
+    }
+    // CDLL() : Head(nullptr), Size(0) {}
+
+    ~CDLL() {
+        while (Head != nullptr) {
+            Node* CurrentNode = Head;
+            Head = Head->GetNextNode();
+            delete CurrentNode;
+        }
+    }
+
+    Node* GetHead() {
+         return Head;
+    }
+
+    int GetSize() {
+        return Size;
+    }
+
+    Node* CreateNode(int NewData) {
+        return new Node(NewData);
+    }
+
+    void DeleteNode(Node* DeleteNode) {
+        delete DeleteNode;
+    }
+    
+    // 링크드 리스트에 노드 추가
+    void AppendNode (Node* NewNode) {
+        if (Head == nullptr) {
+            Head = NewNode;
+            NewNode->SetNextNode(Head);
+            NewNode->SetPrevNode(Head);
+        }
+        else {
+            NewNode->SetNextNode(Head);
+            Head->GetPrevNode()->SetNextNode(NewNode);
+            NewNode->SetPrevNode(Head->GetPrevNode());
+            Head->SetPrevNode(NewNode);
+        }
+        Size++;
+    }
+
+    // 1번 연산 헤드 뽑아내기
+    void RemoveHead() {
+        Node* Temp = Head;
+
+        Head->GetPrevNode()->SetNextNode(Temp->GetNextNode());
+        Head->GetNextNode()->SetPrevNode(Temp->GetPrevNode());
+        Head = Head->GetNextNode();
+
+        Size--;
+    }
+
+    // 2, 3번 연산 좌우로 이동
+    int MoveNode(int Location) {
+        int Left = 0;
+        int Right = 0;
+
+        // 좌측으로 하나씩 이동하는 횟수 카운트
+        Node* CurrentNode = Head;
+        while (CurrentNode->GetData() != Location) {
+            CurrentNode = CurrentNode->GetNextNode();
+            Left++;
+        }
+
+        // 우측으로 하나씩 이동하는 횟수 카운트
+        CurrentNode = Head;
+        while (CurrentNode->GetData() != Location) {
+            CurrentNode = CurrentNode->GetPrevNode();
+            Right++;
+        }
+
+        // 헤드에 탐색을 마친 노드 대입
+        Head = CurrentNode;
+        // 헤드 지워버리고 다음 노드로
+        RemoveHead();
+
+        if (Left < Right) {
+            // cout << "Left : " << Left << "\n";
+            return Left;
+        }
+        else {
+            // cout << "Right : " << Right << "\n";
+            return Right;
+        }
+    }
+    
+    // 전체 노드 프린트 함수
+    void PrintList() {
+        cout << Size << " : ";
+        Node* CurrentNode = Head;
+        for (int i = 0; i < Size; i++) {
+            cout << "(" << CurrentNode->GetPrevNode()->GetData() << ")" << CurrentNode->GetData() << "(" << CurrentNode->GetNextNode()->GetData() << ") ";
+            CurrentNode = CurrentNode->GetNextNode();
+        }
+        cout << "\n";
+    }
+ 
+};
+
+/*
+    처음에 뽑아서 다시 넣는 건 줄 알고 삽질함..
+    RemoveHead()로 뽑아서 버려버림
+*/
+int main() {
+    int N, M, Select;
+    cin >> N >> M;
+
+    CDLL* List = new CDLL();
+
+    for (int i = 0; i < N; i++) {
+        List->AppendNode(List->CreateNode(i + 1));
+    }
+    
+    int Answer = 0;
+    for (int i = 0; i < M; i++) {
+        cin >> Select;
+        if (List->GetHead()->GetData() == Select) {
+            List->RemoveHead();
+        }
+        else {
+            Answer += List->MoveNode(Select);
+        }
+        // List->PrintList();
+    }
+    cout << Answer;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
